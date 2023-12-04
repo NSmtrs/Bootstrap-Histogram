1d94d6ad878a1cc8f19bcaf22817ff85e0772203
NSmtrs <150736717+NSmtrs@users.noreply.github.com>
2023-12-04 14:30:23 -0600

    Update Bootstrap Histogram.py

--- Bootstrap Histogram.py
+++ Bootstrap Histogram.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from plotnine import *
-import os #computer operating system
+import os  # computer operating system
+import numpy as np
 
 class Mileage():
     
-    def __innit__(self, n, n_boot, stat, dat = None):
+    def __init__(self, n, n_boot, stat, dat=None):
         self.n = n
         self.n_boot = n_boot
         self.stat = stat
@@ -16,18 +17,18 @@ class Mileage():
         self.data = dat
         
     def RunSim(self):
-        for i in range(self):
-            boot_sample = self.dat.sample(26, replace = True)
+        for _ in range(self.n):
+            boot_sample = self.data.sample(26, replace=True)
             
             if self.stat == "median":
-                eslf.boot_stat.append(float(boot_sample.median()))
+                self.boot_stat.append(float(boot_sample.median()))
             
-                
             elif self.stat == "mean":
                 self.boot_stat.append(float(boot_sample.mean()))
                 
             elif self.stat == "std dev":
                 self.boot_stat.append(float(boot_sample.std()))
+                
             else:
                 raise TypeError("Wrong statistic name")
         
@@ -39,30 +40,29 @@ class Mileage():
     def Plot_Data(self):
         boot_df = pd.DataFrame({'x': self.boot_stat})
         (
-         ggplot(boot_df, aes(x='x'))+
-         geom_histogram()
+         ggplot(boot_df, aes(x='x')) +
+         geom_histogram(binwidth=1)  # Adjust binwidth as needed
+        )
+    
+    def Conf_Int(self, level=95):
+        lb = (100 - level) / 2
+        ub = (100 - lb)
+        if len(self.boot_stat) > 0:
+            return np.percentile(self.boot_stat, [lb, ub])
+        else:
+            print("Not enough data for a valid answer")
 
-            )
-
-    def Conf_Int(self, level = 95)
-        lb = (1-level)/2
-        ub = (100-lb)
-        if len(self.boot_stat) > 0 :
-            np.percentile(self.boot_stat, [lb, ub])
-
-        else: 
-            print("Not enough data for valid answer")
-
-
-
-os.chdir("C:\\Users\\nsmet\OneDrive\\Documents\\Custom Office Templates")
+# Example usage:
+os.chdir("C:\\Users\\nsmet\\OneDrive\\Documents\\Custom Office Templates")
 dat = pd.read_csv("2017_Fuel_Economy_Data.csv")
-
 dat = dat["Combined Mileage (mpg)"]
 
 n = len(dat)
-
 n_boot = 10_000
-
 stat = "mean"
 
+mileage_simulator = Mileage(n, n_boot, stat, dat)
+mileage_simulator.RunSim()
+mileage_simulator.Plot_Data()
+confidence_interval = mileage_simulator.Conf_Int()
+print("Confidence Interval:", confidence_interval)
